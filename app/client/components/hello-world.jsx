0ac8de07f8da708548a91920ab324f39ad2c6e43
Henson, Garth <Garth.A.Henson@disney.com>
2017-12-21 10:22:26 -0500

    Adding styles and updating the boilerplate content

--- app/client/components/hello-world.jsx
+++ app/client/components/hello-world.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 
-let HelloWorld = ({ name }) => {
-  if (typeof name !== 'undefined') {
-    return (
-      <p>Hello, {name}!</p>
-    );
-  } else {
-    return (
-      <p>Hello, World!</p>
-    );
-  }
+let HelloWorld = () => {
+  return (
+    <div className="container">
+      <section className="header">
+        <h1>Welcome to your React App!</h1>
+        <h2>Your application is up and running.</h2>
+        <a className="button button-primary" target="_blank" href="https://github.com/guahanweb/react-boilerplate/blob/master/README.md">View README</a>
+      </section>
+    </div>
+  );
 };
 
 export default HelloWorld;
