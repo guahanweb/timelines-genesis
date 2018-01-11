edfe8035d28c93fe6ed2e39c9d3363328909977f
Henson, Garth <Garth.A.Henson@disney.com>
2018-01-11 17:57:32 -0500

    GH Pages build

--- js/app.js
+++ js/app.js
@@ -65,254 +65,225 @@
 /************************************************************************/
 /******/ ([
 /* 0 */
-/***/ (function(module, exports) {
+/***/ (function(module, exports, __webpack_require__) {
 
-/**
- * D3 Timeline
- */
+var _genealogies = __webpack_require__(1);
 
-var data = [{
-  name: 'Adam',
-  children: [{
-    name: 'Seth',
-    born: 130
-  }],
-  age: 930
-}, {
-  name: 'Seth',
-  children: [{
-    name: 'Enosh',
-    born: 105
-  }],
-  age: 912
-}, {
-  name: 'Enosh',
-  children: [{
-    name: 'Kenan',
-    born: 90
-  }],
-  age: 905
-}, {
-  name: 'Kenan',
-  children: [{
-    name: 'Mahalalel',
-    born: 70
-  }],
-  age: 910
-}, {
-  name: 'Mahalalel',
-  children: [{
-    name: 'Jared',
-    born: 65
-  }],
-  age: 895
-}, {
-  name: 'Jared',
-  children: [{
-    name: 'Enoch',
-    born: 162
-  }],
-  age: 962
-}, {
-  name: 'Enoch',
-  children: [{
-    name: 'Methuselah',
-    born: 65
-  }],
-  age: 365
-}, {
-  name: 'Methuselah',
-  children: [{
-    name: 'Lamech',
-    born: 187
-  }],
-  age: 969
-}, {
-  name: 'Lamech',
-  children: [{
-    name: 'Noah',
-    born: 182
-  }],
-  age: 777
-}, {
-  name: 'Noah',
-  children: [{
-    name: 'Shem',
-    born: 500
-  }],
-  age: 950
-}, {
-  name: 'Shem',
-  children: [{
-    name: 'Arpachshad',
-    born: 100
-  }],
-  age: 600
-}, {
-  name: 'Arpachshad',
-  children: [{
-    name: 'Shelah',
-    born: 35
-  }],
-  age: 438
-}, {
-  name: 'Shelah',
-  children: [{
-    name: 'Eber',
-    born: 30
-  }],
-  age: 433
-}, {
-  name: 'Eber',
-  children: [{
-    name: 'Peleg',
-    born: 34
-  }],
-  age: 464
-}, {
-  name: 'Peleg',
-  children: [{
-    name: 'Reu',
-    born: 30
-  }],
-  age: 239
-}, {
-  name: 'Reu',
-  children: [{
-    name: 'Serug',
-    born: 32
-  }],
-  age: 239
-}, {
-  name: 'Serug',
-  children: [{
-    name: 'Nahor',
-    born: 30
-  }],
-  age: 230
-}, {
-  name: 'Nahor',
-  children: [{
-    name: 'Terah',
-    born: 29
-  }],
-  age: 148
-}, {
-  name: 'Terah',
-  children: [{
-    name: 'Abram',
-    born: 70
-  }],
-  age: 205
-}, {
-  name: 'Abram',
-  children: [{
-    name: 'Isaac',
-    born: 100
-  }],
-  age: 175
-}, {
-  name: 'Isaac',
-  children: [{
-    name: 'Jacob',
-    born: 60
-  }],
-  age: 180
-}, {
-  name: 'Jacob',
-  children: [{
-    name: 'Joseph',
-    born: 91
-  }],
-  age: 147
-}];
+var _genealogies2 = _interopRequireDefault(_genealogies);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
 
 var year = 0;
-var GENEALOGY = data.map(function (person, i) {
+var genealogies = _genealogies2['default'].map(function (person, i) {
   var data = {
     tir: i + 1,
     name: person.name,
-    start: year,
-    end: year + person.age
+    'class': 'line-' + person.name.toLowerCase(),
+    born: year,
+    child_age: person.birth,
+    child_year: year + person.birth,
+    died: year + person.age,
+    age: person.age
   };
-  year += person.children[0].born;
-
+  year += person.birth;
   return data;
 });
 
-var wrapper = document.querySelector('.chart');
-var width = wrapper.clientWidth;
+var $el = document.querySelector('.chart');
+var wrapper = d3.select($el);
 
-var minX = d3.min(GENEALOGY, function (d) {
-  return d.start;
-});
-var maxX = d3.max(GENEALOGY, function (d) {
-  return d.end;
-});
+var margin = { top: 20, right: 20, bottom: 30, left: 100 };
+var width = $el.clientWidth - margin.left - margin.right;
+var height = $el.clientHeight - margin.top - margin.bottom;
 
 var symbols = d3.nest().key(function (d) {
   return d.tir;
-}).entries(GENEALOGY);
+}).entries(genealogies);
 
-var height = 12;
+var line_height = 22;
 
-var spanX = function spanX(d) {
-  return x(d.start);
-};
-var spanW = function spanW(d) {
-  return x(d.end) - x(d.start);
-};
+var addLineage = function () {
+  var spanX = function spanX(d) {
+    return x(d.died) - x(d.born);
+  };
 
-var x = d3.scaleLinear().domain([-100, 8000]).rangeRound([0, width]);
+  return function (symbol) {
+    var svg = d3.select(this);
+    var index = symbol.key;
+    var lineage = symbol.values[0];
 
-var chart = function chart(symbol) {
-  var svg = d3.select(this);
+    var myX = x(lineage.born);
+    var myY = line_height * (index - 1);
 
-  var holder = svg.selectAll('rect').data(symbol.values).enter();
+    svg.attr('transform', 'translate(' + myX + ',' + myY + ')').attr('class', 'lineage ' + lineage['class']);
 
-  holder.append('rect').attr('x', function (d) {
-    return spanX(d);
-  }).attr('y', 0).attr('width', function (d) {
-    return spanW(d);
-  }).attr('height', height).attr('fill', function (d) {
-    return '#ddf';
-  }).on('mouseover', function (d) {
-    return tooltip.html(getGenealogyInfo(d));
-  }).on('mouseout', function (d) {
-    return tooltip.html('');
-  });
+    svg.append('text').attr('class', 'name').attr('x', -5).attr('y', 8).attr('dy', '.5em').text(lineage.name);
 
-  holder.append('text').attr('x', function (d) {
-    return spanX(d) + 4;
-  }).attr('y', 4).attr('dy', '.5em').text(function (d) {
-    return d.name;
-  });
-};
+    svg.append('rect').attr('class', 'lifeline').attr('height', line_height - 4).attr('width', spanX(lineage));
 
-function getGenealogyInfo(d) {
-  var out = [d.name, 'born year ' + d.start, 'lived to be ' + (d.end - d.start)];
-  return out.join('<br>');
-}
+    svg.append('rect').attr('class', 'before-birth').attr('height', line_height - 4).attr('width', x(lineage.child_age));
 
-var allCharts = d3.select(wrapper).selectAll('svg').data(symbols).enter().append('svg').attr('height', height).each(chart);
+    svg.append('text').attr('class', 'age age-first').attr('transform', 'rotate(-90)').attr('x', -9).attr('y', 11).text(lineage.child_age);
 
-var xAxis = d3.axisBottom(x).ticks(width / 100);
+    var remainder = lineage.age - lineage.child_age;
+    if (remainder > 0) {
+      svg.append('text').attr('class', 'age age-second').attr('transform', 'rotate(-90)').attr('x', -9).attr('y', x(lineage.age) - 7).text(lineage.age - lineage.child_age);
+    }
+  };
+}();
 
-var globalX = d3.select(wrapper).append('svg').attr('class', 'axis').call(xAxis);
+var domain = [0, 2400];
+var domainRange = [0, width];
 
-var catchAll = d3.select('body').append('svg').attr('class', 'zoom').append('rect').attr('fill', 'none').attr('width', width).attr('height', wrapper.getBoundingClientRect().bottom);
+var x = d3.scaleLinear().domain(domain).rangeRound(domainRange);
 
-var tooltip = d3.select(wrapper).append('div').attr('class', 'tooltip');
+var svg = wrapper.append('svg').attr('class', 'main-chart').attr('width', width + margin.left + margin.right).attr('height', height + margin.top + margin.bottom).append('g').attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
 
-catchAll.call(d3.zoom().scaleExtent([0.1, 10]).on('zoom', function () {
-  var transform = d3.event.transform;
-  globalX.call(xAxis.scale(transform.rescaleX(x)));
-  allCharts.selectAll('rect').attr('x', function (d) {
-    return transform.applyX(spanX(d));
-  }).attr('width', function (d) {
-    return transform.k * spanW(d);
-  });
-}));
+// Gridlines
+svg.append('g').attr('class', 'grid').attr('transform', 'translate(0,' + height + ')').call(d3.axisBottom(x).ticks(domain[1] / 100).tickSize(-height).tickFormat(''));
+
+// X Axis
+svg.append('g').attr('transform', 'translate(0, ' + height + ')').call(d3.axisBottom(x));
+
+// Eternity Past
+svg.append('rect').attr('class', 'eternity-past').attr('height', height).attr('width', 80).attr('x', -80);
+
+svg.append('text').attr('text-anchor', 'middle').attr('x', -(height / 2)).attr('y', -30).attr('transform', 'rotate(-90)').text('Eternity Past');
+
+// Gradient
+svg.append('linearGradient').attr('id', 'grayscale-gradient').attr('gradientUnits', 'userSpaceOnUse').attr('x1', -80).attr('y1', 0).attr('x2', 0).attr('y2', 0).selectAll('stop').data([{ offset: '0%', color: '#8d9cb7' }, { offset: '100%', color: '#b3bfd6' }]).enter().append('stop').attr('offset', function (d) {
+  return d.offset;
+}).attr('stop-color', function (d) {
+  return d.color;
+});
+
+// Lineage
+// first, add the holder g, then select all from within it
+var genealogiesChart = svg.append('g').attr('id', 'genealogies-chart');
+
+genealogiesChart.selectAll('g').data(symbols).enter().append('g').attr('class', 'lineage').each(addLineage);
+
+// Drop shadow filter
+var defs = svg.append('defs');
+
+var filter = defs.append('filter').attr('id', 'drop-shadow').attr('height', '120%');
+
+filter.append('feGaussianBlur').attr('in', 'SourceAlpha').attr('stdDeviation', 3).attr('result', 'blur');
+
+filter.append('feOffset').attr('in', 'blur').attr('dx', 4).attr('dy', 4).attr('result', 'offsetBlur');
+
+filter.append('feComponentTransfer').append('feFuncA').attr('type', 'linear').attr('slope', 0.4);
+
+var feMerge = filter.append('feMerge');
+
+feMerge.append('feMergeNode');
+feMerge.append('feMergeNode').attr('in', 'SourceGraphic');
+
+// Info box
+var info = svg.append('rect').attr('class', 'info-box').attr('rx', 4).attr('ry', 4).attr('x', 40).attr('y', 250).style('filter', 'url(#drop-shadow)').attr('height', 240).attr('width', 580);
+
+/***/ }),
+/* 1 */
+/***/ (function(module, exports) {
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+var genealogies = [{
+  name: 'Adam',
+  birth: 130,
+  age: 930
+}, {
+  name: 'Seth',
+  birth: 105,
+  age: 912
+}, {
+  name: 'Enosh',
+  birth: 90,
+  age: 905
+}, {
+  name: 'Kenan',
+  birth: 70,
+  age: 910
+}, {
+  name: 'Mahalalel',
+  birth: 65,
+  age: 895
+}, {
+  name: 'Jared',
+  birth: 162,
+  age: 962
+}, {
+  name: 'Enoch',
+  birth: 65,
+  age: 365
+}, {
+  name: 'Methuselah',
+  birth: 187,
+  age: 969
+}, {
+  name: 'Lamech',
+  birth: 182,
+  age: 777
+}, {
+  name: 'Noah',
+  birth: 500,
+  age: 950
+}, {
+  name: 'Shem',
+  birth: 100,
+  age: 600
+}, {
+  name: 'Arpachshad',
+  birth: 35,
+  age: 438
+}, {
+  name: 'Shelah',
+  birth: 30,
+  age: 433
+}, {
+  name: 'Eber',
+  birth: 34,
+  age: 464
+}, {
+  name: 'Peleg',
+  birth: 30,
+  age: 239
+}, {
+  name: 'Reu',
+  birth: 32,
+  age: 239
+}, {
+  name: 'Serug',
+  birth: 30,
+  age: 230
+}, {
+  name: 'Nahor',
+  birth: 29,
+  age: 148
+}, {
+  name: 'Terah',
+  birth: 70,
+  age: 205
+}, {
+  name: 'Abram',
+  birth: 100,
+  age: 175
+}, {
+  name: 'Isaac',
+  birth: 60,
+  age: 180
+}, {
+  name: 'Jacob',
+  birth: 91,
+  age: 147
+}, {
+  name: 'Joseph',
+  birth: 110,
+  age: 110
+}];
+
+exports['default'] = genealogies;
 
 /***/ })
 /******/ ]);
