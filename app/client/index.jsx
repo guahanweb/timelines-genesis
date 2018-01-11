f390a45fd853ad276f3730d95323ee5239c8e52d
Henson, Garth <Garth.A.Henson@disney.com>
2018-01-11 17:55:50 -0500

    Adding moar nicer graphics.

--- app/client/index.jsx
+++ app/client/index.jsx
@@ -1,262 +1,190 @@
-/**
- * D3 Timeline
- */
-
-const data = [
-  {
-    name: 'Adam',
-    children: [{
-      name: 'Seth',
-      born: 130
-    }],
-    age: 930
-  }, {
-    name: 'Seth',
-    children: [{
-      name: 'Enosh',
-      born: 105
-    }],
-    age: 912
-  }, {
-    name: 'Enosh',
-    children: [{
-      name: 'Kenan',
-      born: 90
-    }],
-    age: 905
-  }, {
-    name: 'Kenan',
-    children: [{
-      name: 'Mahalalel',
-      born: 70
-    }],
-    age: 910
-  }, {
-    name: 'Mahalalel',
-    children: [{
-      name: 'Jared',
-      born: 65
-    }],
-    age: 895
-  }, {
-    name: 'Jared',
-    children: [{
-      name: 'Enoch',
-      born: 162
-    }],
-    age: 962
-  }, {
-    name: 'Enoch',
-    children: [{
-      name: 'Methuselah',
-      born: 65
-    }],
-    age: 365
-  }, {
-    name: 'Methuselah',
-    children: [{
-      name: 'Lamech',
-      born: 187
-    }],
-    age: 969
-  }, {
-    name: 'Lamech',
-    children: [{
-      name: 'Noah',
-      born: 182
-    }],
-    age: 777
-  }, {
-    name: 'Noah',
-    children: [{
-      name: 'Shem',
-      born: 500
-    }],
-    age: 950
-  }, {
-    name: 'Shem',
-    children: [{
-      name: 'Arpachshad',
-      born: 100
-    }],
-    age: 600
-  }, {
-    name: 'Arpachshad',
-    children: [{
-      name: 'Shelah',
-      born: 35
-    }],
-    age: 438
-  }, {
-    name: 'Shelah',
-    children: [{
-      name: 'Eber',
-      born: 30
-    }],
-    age: 433
-  }, {
-    name: 'Eber',
-    children: [{
-      name: 'Peleg',
-      born: 34
-    }],
-    age: 464
-  }, {
-    name: 'Peleg',
-    children: [{
-      name: 'Reu',
-      born: 30
-    }],
-    age: 239
-  }, {
-    name: 'Reu',
-    children: [{
-      name: 'Serug',
-      born: 32
-    }],
-    age: 239
-  }, {
-    name: 'Serug',
-    children: [{
-      name: 'Nahor',
-      born: 30
-    }],
-    age: 230
-  }, {
-    name: 'Nahor',
-    children: [{
-      name: 'Terah',
-      born: 29
-    }],
-    age: 148
-  }, {
-    name: 'Terah',
-    children: [{
-      name: 'Abram',
-      born: 70
-    }],
-    age: 205
-  }, {
-    name: 'Abram',
-    children: [{
-      name: 'Isaac',
-      born: 100
-    }],
-    age: 175
-  }, {
-    name: 'Isaac',
-    children: [{
-      name: 'Jacob',
-      born: 60
-    }],
-    age: 180
-  }, {
-    name: 'Jacob',
-    children: [{
-      name: 'Joseph',
-      born: 91
-    }],
-    age: 147
-  }
-];
+import GENEALOGIES from './data/genealogies';
 
 let year = 0;
-const GENEALOGY = data.map((person, i) => {
+const genealogies = GENEALOGIES.map((person, i) => {
   let data = {
     tir: i + 1,
     name: person.name,
-    start: year, 
-    end: year + person.age
+    class: 'line-' + person.name.toLowerCase(),
+    born: year,
+    child_age: person.birth,
+    child_year: year + person.birth,
+    died: year + person.age,
+    age: person.age
   };
-  year += person.children[0].born;
-
+  year += person.birth;
   return data;
 });
 
-const wrapper = document.querySelector('.chart');
-const width = wrapper.clientWidth;
+const $el = document.querySelector('.chart');
+const wrapper = d3.select($el);
 
-const minX = d3.min(GENEALOGY, d => d.start);
-const maxX = d3.max(GENEALOGY, d => d.end);
+const margin = { top: 20, right: 20, bottom: 30, left: 100 }
+const width = $el.clientWidth - margin.left - margin.right;
+const height = $el.clientHeight - margin.top - margin.bottom;
 
 const symbols = d3.nest()
   .key(d => d.tir)
-  .entries(GENEALOGY);
+  .entries(genealogies);
 
-const height = 12;
+const line_height = 22;
 
-const spanX = d => x(d.start);
-const spanW = d => x(d.end) - x(d.start);
+const addLineage = (function () {
+  const spanX = (d) => x(d.died) - x(d.born);
+
+  return function (symbol) {
+    let svg = d3.select(this);
+    let index = symbol.key;
+    let lineage = symbol.values[0];
+
+    let myX = x(lineage.born);
+    let myY = line_height * (index - 1);
+
+    svg.attr('transform', 'translate(' + myX + ',' + myY + ')')
+      .attr('class', 'lineage ' + lineage.class);
+
+    svg.append('text')
+      .attr('class', 'name')
+      .attr('x', -5)
+      .attr('y', 8)
+      .attr('dy', '.5em')
+      .text(lineage.name);
+
+    svg.append('rect')
+      .attr('class', 'lifeline')
+      .attr('height', line_height - 4)
+      .attr('width', spanX(lineage));
+
+    svg.append('rect')
+      .attr('class', 'before-birth')
+      .attr('height', line_height - 4)
+      .attr('width', x(lineage.child_age));
+
+    svg.append('text')
+      .attr('class', 'age age-first')
+      .attr('transform', 'rotate(-90)')
+      .attr('x', -9)
+      .attr('y', 11)
+      .text(lineage.child_age);
+
+    let remainder = lineage.age - lineage.child_age;
+    if (remainder > 0) {
+      svg.append('text')
+        .attr('class', 'age age-second')
+        .attr('transform', 'rotate(-90)')
+        .attr('x', -9)
+        .attr('y', x(lineage.age) - 7)
+        .text(lineage.age - lineage.child_age);
+    }
+  };
+})();
+
+const domain = [0, 2400];
+const domainRange = [0, width];
 
 const x = d3.scaleLinear()
-  .domain([-100, 8000])
-  .rangeRound([0, width]);
+  .domain(domain)
+  .rangeRound(domainRange);
 
-const chart = function(symbol) {
-  let svg = d3.select(this);
+const svg = wrapper.append('svg')
+    .attr('class', 'main-chart')
+    .attr('width', width + margin.left + margin.right)
+    .attr('height', height + margin.top + margin.bottom)
+  .append('g')
+    .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
 
-  let holder = svg.selectAll('rect')
-    .data(symbol.values)
-    .enter();
+// Gridlines
+svg.append('g')
+  .attr('class', 'grid')
+  .attr('transform', 'translate(0,' + height + ')')
+  .call(d3.axisBottom(x)
+    .ticks(domain[1] / 100)
+    .tickSize(-height)
+    .tickFormat('')
+  );
 
-  holder.append('rect')
-      .attr('x', d => spanX(d))
-      .attr('y', 0)
-      .attr('width', d => spanW(d))
-      .attr('height', height)
-      .attr('fill', d => '#ddf')
-    .on('mouseover', d => tooltip.html(getGenealogyInfo(d)))
-    .on('mouseout', d => tooltip.html(''));
+// X Axis
+svg.append('g')
+  .attr('transform', 'translate(0, ' + height + ')')
+  .call(d3.axisBottom(x));
 
-  holder.append('text')
-    .attr('x', d => spanX(d) + 4)
-    .attr('y', 4)
-    .attr('dy', '.5em')
-    .text(d => d.name);
-};
+// Eternity Past
+svg.append('rect')
+  .attr('class', 'eternity-past')
+  .attr('height', height)
+  .attr('width', 80)
+  .attr('x', -80);
 
-function getGenealogyInfo(d) {
-  let out = [
-    d.name,
-    'born year ' + d.start,
-    'lived to be ' + (d.end - d.start)
-  ];
-  return out.join('<br>');
-}
+svg.append('text')
+  .attr('text-anchor', 'middle')
+  .attr('x', -(height / 2))
+  .attr('y', -30)
+  .attr('transform', 'rotate(-90)')
+  .text('Eternity Past');
 
-const allCharts = d3.select(wrapper).selectAll('svg')
+// Gradient
+svg.append('linearGradient')
+  .attr('id', 'grayscale-gradient')
+  .attr('gradientUnits', 'userSpaceOnUse')
+  .attr('x1', -80).attr('y1', 0)
+  .attr('x2', 0).attr('y2', 0)
+  .selectAll('stop')
+    .data([
+      { offset: '0%', color: '#8d9cb7' },
+      { offset: '100%', color: '#b3bfd6' }
+    ])
+    .enter().append('stop')
+      .attr('offset', d => d.offset)
+  .attr('stop-color', d => d.color);
+
+// Lineage
+// first, add the holder g, then select all from within it
+const genealogiesChart = svg.append('g')
+  .attr('id', 'genealogies-chart');
+
+genealogiesChart.selectAll('g')
   .data(symbols)
   .enter()
-  .append('svg')
-    .attr('height', height)
-  .each(chart);
+  .append('g')
+    .attr('class','lineage')
+  .each(addLineage);
 
-const xAxis = d3.axisBottom(x)
-  .ticks(width / 100);
+// Drop shadow filter
+const defs = svg.append('defs');
 
-const globalX = d3.select(wrapper)
-  .append('svg')
-    .attr('class', 'axis')
-    .call(xAxis);
+const filter = defs.append('filter')
+  .attr('id', 'drop-shadow')
+  .attr('height', '120%');
 
-const catchAll = d3.select('body')
-  .append('svg')
-    .attr('class', 'zoom')
-  .append('rect')
-    .attr('fill', 'none')
-    .attr('width', width)
-  .attr('height', wrapper.getBoundingClientRect().bottom);
+filter.append('feGaussianBlur')
+  .attr('in', 'SourceAlpha')
+  .attr('stdDeviation', 3)
+  .attr('result', 'blur');
 
-const tooltip = d3.select(wrapper).append('div')
-  .attr('class', 'tooltip');
+filter.append('feOffset')
+  .attr('in', 'blur')
+  .attr('dx', 4)
+  .attr('dy', 4)
+  .attr('result', 'offsetBlur');
 
-catchAll.call(d3.zoom()
-  .scaleExtent([0.1, 10])
-  .on('zoom', () => {
-    let transform = d3.event.transform;
-    globalX.call(xAxis.scale(transform.rescaleX(x)));
-    allCharts.selectAll('rect')
-      .attr('x', d => transform.applyX(spanX(d)))
-      .attr('width', d => transform.k * spanW(d));
-  })
-);
+filter.append('feComponentTransfer')
+  .append('feFuncA')
+    .attr('type', 'linear')
+    .attr('slope', 0.4);
+
+const feMerge = filter.append('feMerge');
+
+feMerge.append('feMergeNode');
+feMerge.append('feMergeNode')
+  .attr('in', 'SourceGraphic');
+
+// Info box
+const info = svg.append('rect')
+  .attr('class', 'info-box')
+  .attr('rx', 4)
+  .attr('ry', 4)
+  .attr('x', 40)
+  .attr('y', 250)
+  .style('filter', 'url(#drop-shadow)')
+  .attr('height', 240)
+  .attr('width', 580);
